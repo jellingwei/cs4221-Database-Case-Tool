657dcf7596b86e808560d86cd1168fa8d28b6c13
User <tongchunkit@gmail.com>
2015-03-13 10:12:40 +0800

    Updated LTK pseudocode comments

--- CaseTool/ltk.cpp
+++ CaseTool/ltk.cpp
@@ -9,10 +9,6 @@ namespace ltk {
 	using std::exception;
 	using std::unordered_map;
 
-	set<AttributeSet> deletionNormalization(AttributeSet, set<FunctionalDependency>) {
-		throw exception();
-	}
-
 	set<AttributeSet> constructPreparatorySchema(AttributeSet startAttributes, set<FunctionalDependency> startFDs) {
 		set<FunctionalDependency> singletonFDs = bernstein::decomposeFd(startFDs);
 		set<FunctionalDependency> nonRedundantAttributesFDs = bernstein::removeRedundantAttributes(singletonFDs);
@@ -24,10 +20,24 @@ namespace ltk {
 	}
 
 	set<AttributeSet> augmentPreparatorySchema(set<AttributeSet> constructedRelations, set<FunctionalDependency> minimalFDs) {
+		//For each constructed relation, check if the closure of attributes can give back the whole initial set of attributes
+		//If cannot, construct a subset of the initial set of attributes such that its closure can give it back
+		//Add this relation to the preparatory schema to give the whole preparatory schema
 		throw exception();
 	}
 
-	AttributeSet superfluousAttributeDetection(set<AttributeSet>, AttributeSet) {
+	//Outputs the new set of synthesized key of relation if attribute is superfluous
+	//Otherwise outputs null
+	AttributeSet superfluousAttributeDetection(set<AttributeSet> preparatoryRelation, AttributeSet singleAttribute) {
+		//Mark attribute as superfluous
+		throw exception();
+	}
+
+	set<AttributeSet> deletionNormalization(AttributeSet attributes, set<FunctionalDependency> FDs) {
+		//Construct preparatory schema from above
+		//For each relation, for each attribute, 
+		//if superfluous attribute detection returns a nonempty set, construct a new relation and replace in preparatory schema
+		//Final output is in LTK normal form
 		throw exception();
 	}
 }
--- CaseTool/ltk.h
+++ CaseTool/ltk.h
@@ -6,7 +6,7 @@
 namespace ltk {
 	using std::set;
 
-	set<AttributeSet> deletionNormalization(AttributeSet, set<FunctionalDependency>);
 	set<AttributeSet> constructPreparatorySchema(AttributeSet, set<FunctionalDependency>);
 	AttributeSet superfluousAttributeDetection(set<AttributeSet>, AttributeSet);
+	set<AttributeSet> deletionNormalization(AttributeSet, set<FunctionalDependency>);
 }
